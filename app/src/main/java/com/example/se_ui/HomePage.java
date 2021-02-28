d20d18b436a8ad435cef150acaea0cdacbcad88f
Harsh Agrawal <43035708+harshag24@users.noreply.github.com>
2021-02-28 18:31:23 +0530

    Upload from Gallery Working

--- app/src/main/java/com/example/se_ui/HomePage.java
+++ app/src/main/java/com/example/se_ui/HomePage.java
@@ -1,18 +1,42 @@
 package com.example.se_ui;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.webkit.MimeTypeMap;
+import android.widget.Button;
 import android.widget.ImageButton;
-
+import android.widget.ImageView;
+import android.widget.Toast;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
 
 public class HomePage extends AppCompatActivity {
     ImageButton logout;
     BottomNavigationView navigation;
-
+    Button choosepic  , next;
+    ImageView selectedpic;
+    ProgressDialog progressDialog;
+    StorageReference mStorageRef;
+    Uri selectedImage;
+    FirebaseUser user;
+    DatabaseReference databaseReference ;
+    StorageReference Ref;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +55,85 @@ public class HomePage extends AppCompatActivity {
             finish();
             startActivity(intent);
         });
+
+        mStorageRef= FirebaseStorage.getInstance().getReference("Images");
+        progressDialog = new ProgressDialog(this);
+        selectedpic = findViewById(R.id.selectedpic_imageview);
+        choosepic = findViewById(R.id.selectpic_button);
+        next = findViewById(R.id.nextTopage3_button);
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        mStorageRef=FirebaseStorage.getInstance().getReference();
+
+        choosepic.setOnClickListener(v -> {
+            Intent galleryintent = new Intent(Intent.ACTION_PICK , MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(galleryintent , 1 );
+
+            next.setOnClickListener(v1 -> {
+                progressDialog.setMessage("Uploading...");
+                progressDialog.show();
+                Fileuploader();
+            });
+
+        });
+    }
+
+    private String getExtension(Uri uri)
+    {
+        ContentResolver cr = getContentResolver();
+        MimeTypeMap mimeTypeMap= MimeTypeMap.getSingleton();
+        return mimeTypeMap.getExtensionFromMimeType(cr.getType(uri));
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode ==1 &&resultCode== RESULT_OK && data!=null)
+        {
+            selectedImage = data.getData();
+            selectedpic.setImageURI(selectedImage);
+        }
+    }
+
+    public void Fileuploader()
+    {
+        Ref = mStorageRef.child(System.currentTimeMillis()+"."+getExtension(selectedImage));
+        Ref.putFile(selectedImage)
+                .addOnSuccessListener(taskSnapshot -> {
+//                    Toast.makeText(HomePage.this,"Successfully Uploaded",Toast.LENGTH_LONG).show();
+                    Log.e("TAG", "onClick: "+user.getUid() );
+//                  pred();
+                    String[] url = addPrediction();
+                    progressDialog.dismiss();
+                    Intent intent = new Intent(HomePage.this , Result.class);
+                    intent.putExtra("URL", url[0]);
+                    finish();
+                    startActivity(intent);
+                })
+                .addOnFailureListener(exception -> Toast.makeText(HomePage.this,"Upload Unsuccessful!! Please Try Again ",Toast.LENGTH_LONG).show());
+    }
+
+    public String[] addPrediction()
+    {
+        final String[] url_main = new String[1];
+        Ref.getDownloadUrl().addOnSuccessListener(uri -> {
+            Log.d("TAG", String.valueOf(uri));
+
+            String url = String.valueOf(uri);
+            url_main[0] = url;
+            String  current_date;
+            Calendar calendar = Calendar.getInstance();
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd-HH:mm:ss:SSS");
+            current_date = simpleDateFormat.format(calendar.getTime());
+
+            HashMap<String, Object> add_product = new HashMap<>();
+//          add_product.put("prediction" , prediction);
+            add_product.put("url" , url);
+
+            databaseReference = FirebaseDatabase.getInstance().getReference().child("Prediction").child(user.getUid()).child(current_date);
+            databaseReference.updateChildren(add_product);
+        });
+        return url_main;
     }
 
     private final BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
